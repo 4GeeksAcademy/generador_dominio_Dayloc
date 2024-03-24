3b83e8e1456cc1f8b3e3e8be7d8513681ecbd774
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-03-24 08:39:04 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es"];
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[0].concat(adj[i], noun[0], extension[0]));
+    console.log(pronoun[1].concat(adj[0], noun[0], extension[0]));
+  }
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[0].concat(adj[i], noun[0], extension[1]));
+    console.log(pronoun[1].concat(adj[0], noun[0], extension[1]));
+  }
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[0].concat(adj[i], noun[1], extension[0]));
+    console.log(pronoun[1].concat(adj[0], noun[1], extension[0]));
+  }
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[0].concat(adj[i], noun[1], extension[1]));
+    console.log(pronoun[1].concat(adj[0], noun[1], extension[1]));
+  }
 };
